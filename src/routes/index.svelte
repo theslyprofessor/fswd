e93775e62cbd52303fd97471490022523832d041
theslyprofessor <ntiruviluamala@gmail.com>
2021-12-16 23:05:15 +0000

    linters and formatters

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,16 @@
 <script>
-    import TodoItem from '$lib/todo-item.svelte'
-
-    const title = 'Todo'
+	import TodoItem from '$lib/todo-item.svelte'
+	const title = 'Todo'
 </script>
 
 <svelte:head>
-    <title>{title}</title>
+	<title>{title}</title>
 </svelte:head>
 
 <h1>{title}</h1>
 
 <form action="" method="">
-  <input 
-    type="text" 
-    name="text" 
-    aria-label="Add a todo" 
-    placeholder="+ type to add a todo"
-  >
+	<input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo" />
 </form>
 
 <TodoItem />
