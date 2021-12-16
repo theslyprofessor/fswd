e6c7e3fb314ff9749b435f291d06c70a2d2e1fb0
theslyprofessor <ntiruviluamala@gmail.com>
2021-12-16 09:22:17 +0000

    Develop HTML structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="" />
+        <button aria-label="Mark done/not done">Done/NotDone</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="Save to do">Save</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="Delete">Delete</button>
+    </form>
+</div>
